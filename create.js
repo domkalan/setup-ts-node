9f8e365c32db34a7c3f4de24a9f1a4edc83364ca
Kalan Dominick <domkalan@gmail.com>
2025-06-22 23:43:32 -0700

    New console messages, remove create.js file

--- create.js
+++ create.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 const { execSync } = require("child_process");
 const degit = require("degit");
+const { unlinkSync } = require("fs");
 
 const repo = "https://github.com/domkalan/ts-node-boilerplate";
 const targetDir = process.argv[2] || ".";
@@ -10,12 +11,16 @@ console.log(`Scaffolding ts-node-boilderplate project in ${targetDir}...`);
 const degitEmitter = degit(repo, { cache: false, force: true });
 
 degitEmitter.clone(targetDir).then(() => {
-    console.log("✅ Done! Now run:");
-    console.log(`  cd ${targetDir}`);
-    console.log("  npm install");
-    console.log("  npm start");
+    console.log("✅ Done! Your project has been created successfully.");
+    console.log(`  Project located at: ${targetDir}`);
 
+    // install dependencies
+    console.log("Installing dependencies...");
     execSync('npm i', { cwd: targetDir });
+
+    // remove the create.js script
+    console.log("Removing the create.js script...");
+    unlinkSync(`${targetDir}/create.js`);
 
     process.exit(0);
 }).catch(err => console.error("❌ Error:", err));
